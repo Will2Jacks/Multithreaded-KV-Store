70cfd1549bab8e85741f08498dbe5841159d13a7
Will2Jacks <sohamdascs23@kgpian.iitkgp.ac.in>
2026-02-24 11:10:05 +0000

    CMakeLists.txt added

---
+++ build/CMakeFiles/kvserver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kvserver.dir/src/main.cpp.o"
+  "CMakeFiles/kvserver.dir/src/main.cpp.o.d"
+  "kvserver"
+  "kvserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kvserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
